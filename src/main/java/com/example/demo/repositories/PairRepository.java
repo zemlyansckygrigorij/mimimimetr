4f93aa85b4ed64967cd0ee887ae5ad8bbc7fb1ea
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-08 14:58:54 +0500

    delete class PairRepository

--- src/main/java/com/example/demo/repositories/PairRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Pair;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PairRepository  extends CrudRepository<Pair, Long> {
-}
