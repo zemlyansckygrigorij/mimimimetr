ea025f4e12bbb0264168cd1cbf6c397acbeb9af9
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-08 16:36:26 +0500

    update class CatRepository

--- src/main/java/com/example/demo/repository/CatRepository.java
+++ src/main/java/com/example/demo/repository/CatRepository.java
@@ -1,7 +1,8 @@
 package com.example.demo.repository;
-
+/*
 import com.example.demo.entities.Cat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CatRepository extends JpaRepository<Cat, Long> {
 }
+*/
