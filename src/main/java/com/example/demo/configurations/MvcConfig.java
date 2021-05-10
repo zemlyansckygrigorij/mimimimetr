9fb600da5e9290a14dc779f4c27da45ed42df034
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-10 14:00:11 +0500

    update class class MvcConfig add registry.addViewController("/login").setViewName("login");

--- src/main/java/com/example/demo/configurations/MvcConfig.java
+++ src/main/java/com/example/demo/configurations/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/403").setViewName("403");
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
     }
 
 }
